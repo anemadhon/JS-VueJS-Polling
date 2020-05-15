b7722e15857b83159a972ce1d489b01ca669b77f
anemadhon <anemadhon@gmail.com>
2020-05-15 15:56:45 +0700

    menambah animasi saat transisi di respon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,6 +36,13 @@ export default {
 </script>
 
 <style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+
   .container{
     display: flex;
     flex-wrap: wrap;
